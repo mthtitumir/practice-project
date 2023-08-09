7760f0c0bb498f9d4f1227857d9564d2841414bc
afrozamitu <afrozamitulive@gmail.com>
2023-08-09 22:36:04 +0600

    new text

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,7 +1,11 @@
 const Services = () => {
     return (
         <div>
-            <h1>It's the service section</h1>
+            <h1>It is the service section</h1>
+            <h1>It is the service section</h1>
+            <h1>It is the service section</h1>
+            <h1>It is the service section</h1>
+            <h1>It is the service section</h1>
         </div>
     );
 };
